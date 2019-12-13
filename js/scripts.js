707e6acacc61539ac175eb9439673b22cc76e315
Jonathan <gator92432@gmail.com>
2019-12-12 20:50:23 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var pizza = 5;
+var soda = 1;
+var bread = 3;
+
+var total = pizza + soda;
+
+console.log(total);
+
+const name = 'Jonathan Gaither';
+
+const welcome = "Welcome to Geekwise Academy";
+
+
+console.log(name);
+console.log(welcome);
+
+//Array
+var fruits = ['apples', 'grapes', 'oranges', 'bananas']
+var rockBand = ['Pantera', 'Kiss', 'Nickelback'];
+
+console.log(fruits);
+console.log(rockBand);
+
+//object
+
+var car = {
+    type: "Fiat",
+    model: "500",
+    color: "white"
+};
